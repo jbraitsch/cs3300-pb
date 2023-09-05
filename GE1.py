0a766754b08a0184740bed7ec59bbfb3596087e9
jbraitsch <joe.braitsch3@gmail.com>
2023-09-05 10:50:47 -0600

    merge take 2

--- GE1.py
+++ GE1.py
@@ -1,4 +1,5 @@
 print("Team Puss in Boots")
 print("Joe getting it started")
 print("Nate struggling with Git but he's here")
-pritn("Joe is merging too")
+print("Luke")
+print("Joe is merging too")
